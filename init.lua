8a7561cd24d0c87d26ca47d4a6f13f7874d25886
Max Dondukov <mdondukov@pm.me>
2025-10-20 12:57:27 +0600

    chroe: Add render-markdown.nvim

--- init.lua
+++ init.lua
@@ -1096,8 +1096,7 @@ require('lazy').setup({
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
